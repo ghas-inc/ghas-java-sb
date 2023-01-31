b5600bc74c8a3aa8bc56724e523b02962da22dab
Reinier Timmer <reinier.timmer@ah.nl>
2023-01-31 16:04:20 +0100

    added some vulnerabilities

--- src/main/java/com/example/ghasjavasb/Customer.java
+++ src/main/java/com/example/ghasjavasb/Customer.java
@@ -1,6 +1,20 @@
 package com.example.ghasjavasb;
 
 class Customer {
+  private final String name;
+
+  public Customer(String name) {
+    this.name = name;
+  }
+
+	public String toUri() {
+		return "https://localhost:2347/" + this.name;
+	}
+
+	public String toURI() {
+		return "https://localhost:2347/" + this.name;
+	}  
+
 	public boolean equals(Object o) {
 		if (o == null) return false;
 		if (Customer.class != o.getClass()) return false;
@@ -9,4 +23,5 @@ class Customer {
 		if (id != id) return false;  // CodeQL - should trigger "comparison of identical values"
 		return true;
 	}
+
 }
--- src/main/java/com/example/ghasjavasb/GhasJavaSbApplication.java
+++ src/main/java/com/example/ghasjavasb/GhasJavaSbApplication.java
@@ -8,6 +8,16 @@ public class GhasJavaSbApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GhasJavaSbApplication.class, args);
+
+		// GHAS DEMO
+		try {
+			String latlonCoords = args[1];
+			Runtime rt = Runtime.getRuntime();
+			Process exec = rt.exec("cmd.exe /C latlon2utm.exe " + latlonCoords);		
+		}
+		catch (Exception e) {
+
+		}
 	}
 
 }
