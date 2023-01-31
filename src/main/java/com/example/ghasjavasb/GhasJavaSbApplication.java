99ecd418892c2f2afb5f285d197f8992c54a91cc
Reinier Timmer <reinier.timmer@ah.nl>
2023-01-31 15:20:59 +0100

    initial commit

---
+++ src/main/java/com/example/ghasjavasb/GhasJavaSbApplication.java
@@ -0,0 +1,13 @@
+package com.example.ghasjavasb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhasJavaSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhasJavaSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ghasjavasb/GhasJavaSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ghasjavasb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhasJavaSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
